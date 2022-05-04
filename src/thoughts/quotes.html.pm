cc5158cc5ade469172b32c5e341bc808fcdfcddb
rymaju <ryan.matthew.jung@gmail.com>
2022-05-04 23:59:58 +0000

    Deploying to main from @ rymaju/pollen-ryanjung.dev@2b6fca5d3bc3e6c2ec7b5ec85c695f21a08d374a 🚀

--- src/thoughts/quotes.html.pm
+++
@@ -1,6 +0,0 @@
-#lang pollen
-◊(define-meta title "Quotes")
-◊(define-meta desc "A collection of quotes on programming and computer science.")
-◊(for/splice ([q quotes])
-(p(quote-block #:source (quote-text-attribution q) (quote-text-content q))))
-
